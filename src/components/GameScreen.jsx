b7ff0d49f491b239aac91891e6de12b704456012
white731 <whitesideslandon@gmail.com>
2024-04-07 12:47:34 -0600

    added array randomizer

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -13,12 +13,20 @@ export const GameScreen = () => {
   const [storeAssets, setStoreAssets] = useState([]);
   const navigate = useNavigate();
 
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
   const getRecords = async () => {
     let app_key =
       "pat5lPwuYvtAqOvlM.b863d9705446e58947a355c72e7166014adef0875bfa23226a740e353b032a85";
     let app_id = "apppyyL3422VTkvRC";
-    let view = "Assets";
-    let url = "https://api.airtable.com/v0/" + app_id + "/" + view;
+    let table = "Assets";
+    let url = "https://api.airtable.com/v0/" + app_id + "/" + table;
     let axiosConfig = {
       headers: {
         Authorization: "Bearer " + app_key,
@@ -29,7 +37,9 @@ export const GameScreen = () => {
     try {
       let res = await axios.get(url, axiosConfig);
       let data = res.data.records;
-      let top25 = data.slice(0, 25);
+      console.log(data);
+      let shuffledArray = shuffleArray([...data]);
+      let top25 = shuffledArray.slice(0, 25);
       setStoreAssets(top25);
       console.log(top25);
     } catch (error) {}
